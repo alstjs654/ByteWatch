e813e680c9a35ac72257c90e1eb84c23e4af34d8
USER-506 <USER-506@DESKTOP-U2NEC3I>
2021-07-20 16:11:14 +0900

    예

---
+++ ByteWatch/src/main/java/drama/drama_able.java
@@ -0,0 +1,9 @@
+package drama;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface drama_able {
+
+	public String service(HttpServletRequest request,HttpServletResponse Response);
+}
